ee05ec3fe7c654e70afe219f95c754062959b421
Crislaine Luana <crislaindepaulalu@gmail.com>
2025-12-05 09:57:11 -0300

    Adicionar arquivos de projeto.

---
+++ ApiContatos10/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiContatos10.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiContatos10.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
---
+++ ApiContatos10/Models/Contato.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiContatos10.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Endereco { get; set; }
+        public string Categoria { get; set; }
+    }
+}
